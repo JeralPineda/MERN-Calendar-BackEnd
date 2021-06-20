7dd0b62923b17779325e94d314e2dd54e7b117af
Jeral <jeral.cris@gmail.com>
2021-06-20 16:34:44 -0600

    routes auth

---
+++ routes/auth.js
@@ -0,0 +1,17 @@
+/*
+    Rutas de usuarios / auth
+    host: api/auth
+*/
+
+const { Router } = require('express');
+const { crearUsuario, loginUsuario, revalidarToken } = require('../controllers/auth');
+
+const router = Router();
+
+router.post('/new', crearUsuario);
+
+router.post('/', loginUsuario);
+
+router.get('/renew', revalidarToken);
+
+module.exports = router;
