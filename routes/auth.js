cacc7de53fedd1a9861f4edceb4918d1c311c823
Jeral <jeral.cris@gmail.com>
2021-06-20 17:42:40 -0600

    validar los campos con middleware y express-validator

--- routes/auth.js
+++ routes/auth.js
@@ -4,13 +4,35 @@
 */
 
 const { Router } = require('express');
+const { check } = require('express-validator');
+
 const { crearUsuario, loginUsuario, revalidarToken } = require('../controllers/auth');
+const { validarCampos } = require('../middlewares/validar-campos');
 
 const router = Router();
 
-router.post('/new', crearUsuario);
+router.post(
+   '/new',
+   [
+      //Midlewares
+      check('name', 'El nombre es obligatorio').not().isEmpty(),
+      check('email', 'El correo es obligatorio').isEmail(),
+      check('password', 'El password debe contener al menos 6 caracteres').isLength({ min: 6 }),
+      validarCampos,
+   ],
+   crearUsuario
+);
 
-router.post('/', loginUsuario);
+router.post(
+   '/',
+   [
+      //Midlewares
+      check('email', 'El correo es obligatorio').isEmail(),
+      check('password', 'El password debe contener al menos 6 caracteres').isLength({ min: 6 }),
+      validarCampos,
+   ],
+   loginUsuario
+);
 
 router.get('/renew', revalidarToken);
 
