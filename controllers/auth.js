1f4f476dc37d3abe904e893f610d1d190968aa9c
Jeral <jeral.cris@gmail.com>
2021-06-20 16:51:43 -0600

    obtenemos informacion de los tres endpoints

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,14 +1,23 @@
 const { response } = require('express');
 
 const crearUsuario = (req, res = response) => {
+   const { name, email, password } = req.body;
    res.json({
+      ok: true,
       msg: 'registro',
+      name,
+      email,
+      password,
    });
 };
 
 const loginUsuario = (req, res = response) => {
+   const { email, password } = req.body;
    res.json({
+      ok: true,
       msg: 'login',
+      email,
+      password,
    });
 };
 
