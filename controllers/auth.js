0672823df2454b784cbbe3962cb422a2ebc34971
Jeral <jeral.cris@gmail.com>
2021-06-20 17:41:40 -0600

    Cambiando el estado de la response para crear usuarios

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,7 +2,7 @@ const { response } = require('express');
 
 const crearUsuario = (req, res = response) => {
    const { name, email, password } = req.body;
-   res.json({
+   res.status(201).json({
       ok: true,
       msg: 'registro',
       name,
