c07370a6e81ec034e1344e1144933475af9e0933
Jeral <jeral.cris@gmail.com>
2021-06-20 23:00:24 -0600

    endpoint para revalidar el token y crear uno nuevo

--- controllers/auth.js
+++ controllers/auth.js
@@ -89,9 +89,15 @@ const loginUsuario = async (req, res = response) => {
    }
 };
 
-const revalidarToken = (req, res = response) => {
+const revalidarToken = async (req, res = response) => {
+   const { uid, name } = req;
+
+   //    Generar un nuevo JWT y retornarlo en esta nueva petición
+   const token = await generarJWT(uid, name);
+
    res.json({
-      msg: 'revalidar',
+      ok: true,
+      token,
    });
 };
 
