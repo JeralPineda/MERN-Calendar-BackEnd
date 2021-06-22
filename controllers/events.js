f37826a9570b95c4a479578fb6dce4e21522dbc5
Jeral <jeral.cris@gmail.com>
2021-06-22 00:01:59 -0600

    Endpoint para actualizar un evento

--- controllers/events.js
+++ controllers/events.js
@@ -42,11 +42,53 @@ const crearEvento = async (req, res = response) => {
    }
 };
 
-const actualizarEvento = (req, res = response) => {
-   res.json({
-      ok: true,
-      msg: 'Actualizar evento',
-   });
+const actualizarEvento = async (req, res = response) => {
+   // obtener el id desde el url
+   const eventoId = req.params.id;
+   const { uid } = req; //extraemos el uid
+
+   try {
+      const evento = await Evento.findById(eventoId);
+
+      console.log(evento);
+
+      //   Verificar que el evento existe
+      if (!evento) {
+         res.status(404).json({
+            ok: false,
+            msg: 'Evento no existe por ese id',
+         });
+      }
+
+      //   Verificar si la persona que creo el evento es el mismo que lo desea actualizar
+      if (evento.user.toString() !== uid) {
+         res.status(401).json({
+            ok: false,
+            msg: 'No tiene privilegio de editar este evento',
+         });
+      }
+
+      const nuevoEvento = {
+         ...req.body,
+         user: uid,
+      };
+
+      //   Actualizamos el evento
+      const eventoActualizado = await Evento.findByIdAndUpdate(eventoId, nuevoEvento, { new: true });
+
+      // new: true => es para que devuelva el evento actualizado que se acab de ingresar y no el anterior, que lo muestra por defecto para comparación
+
+      res.json({
+         ok: true,
+         evento: eventoActualizado,
+      });
+   } catch (error) {
+      console.log(error);
+      res.status(500).json({
+         ok: false,
+         msg: 'Hable con el administrador',
+      });
+   }
 };
 
 const eliminarEvento = (req, res = response) => {
