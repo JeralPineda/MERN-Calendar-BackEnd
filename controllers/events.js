e160df1aad8a417e3f521622c2a96f15d2e62755
Jeral <jeral.cris@gmail.com>
2021-06-21 20:37:37 -0600

    Controller con cada uno de los endpoints del CRUD de los eventos

---
+++ controllers/events.js
@@ -0,0 +1,36 @@
+const { response } = require('express');
+
+const getEventos = (req, res = response) => {
+   res.json({
+      ok: true,
+      msg: 'Obtener eventos',
+   });
+};
+
+const crearEvento = (req, res = response) => {
+   res.json({
+      ok: true,
+      msg: 'Crear evento',
+   });
+};
+
+const actualizarEvento = (req, res = response) => {
+   res.json({
+      ok: true,
+      msg: 'Actualizar evento',
+   });
+};
+
+const eliminarEvento = (req, res = response) => {
+   res.json({
+      ok: true,
+      msg: 'Eliminar evento',
+   });
+};
+
+module.exports = {
+   getEventos,
+   crearEvento,
+   actualizarEvento,
+   eliminarEvento,
+};
